39950d5eb06b8bf82574aeb0cbe11bd8016354a8
Cristina Cordero <ccordero@opentrends.net>
2022-01-23 20:30:51 +0100

    en php per que es llisti tot sol

---
+++ artistes.php
@@ -0,0 +1,17 @@
+<?php 
+return array(
+    'alfaro' => 'Greta Alfaro',
+    'carreras' => 'Anna Carreras',
+    'ayllu' => 'Ayllu',
+    'coma' => 'Maria Coma',
+    'girones' => 'Albert Gironès',
+    'jose' => 'José y sus hermanas',
+    'lahitte' => 'Verónica Lahitte + Antonio Gagliano',
+    'llimos' => 'Robert Llimós',
+    'morales' => 'Cristina Morales',
+    'muaj' => 'Muaj!',
+    'ortiz' => 'Agustín Ortiz Herrera',
+    'rikic' => 'Mónica Rikić',
+    'salva' => 'Xesca Salvà',
+    'soto' => 'Montserrat Soto'
+);
