6d81252555f301aec54b98562d8f4081af120a54
Cristina Cordero <ccordero@opentrends.net>
2022-01-23 13:42:05 +0100

    noves fotos 22-01-22

--- renamefiles.sh
+++ renamefiles.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 shopt -s globstar  ##globstar will let us match files recursively
-files=( /home/ccordero/imaginaria-git/santamonica/entreacte/*/*.jpg )  ##Array containing matched files, mention where to search and what files here
+files=( ./*/*.jpg )  ##Array containing matched files, mention where to search and what files here
+old=""
+foo=1
 for i in "${files[@]}"; do 
+    echo $i
     d="${i%/*}"  ##Parameter expansion, gets the path upto the parent directory
     d_="${d##*/}"  ##gets the name of parent directory
+    if [ "$old" = "$d_" ];
+    then 
+        foo=$((foo+1)) 
+    else
+        old=${d_}
+        foo=1
+    fi
     f="${i##*/}"  ##gets the file name
-        mv -n "$i" "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d_%H%M%S").jpg"
+        #echo "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d" )_"${foo}".jpg"
+        mv -n "$i" "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d" )_"${foo}".jpg"
 done
