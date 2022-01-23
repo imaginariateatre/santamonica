3d2c222048f1dcccfbc6952bccbf571c33141715
Cristina Cordero <ccordero@opentrends.net>
2022-01-23 20:02:10 +0100

    prova php

--- alfaro/index.php
+++ alfaro/index.php
@@ -4,5 +4,13 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+
+ <?php 
+  $mydir = 'dummy'; 
+  
+  $myfiles = array_diff(scandir($mydir), array('.', '..')); 
+  
+  print_r($myfiles); 
+?>
  </body>
 </html>
