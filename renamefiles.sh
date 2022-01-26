1c03a4937c913d3defe5a6352ebecb157a755d96
Cristina Cordero <ccordero@opentrends.net>
2022-01-26 20:35:11 +0100

    fotos d'avui

--- renamefiles.sh
+++ renamefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s globstar  ##globstar will let us match files recursively
-files=( ./*/*.jpg )  ##Array containing matched files, mention where to search and what files here
+files=( ./*/WhatsApp*.jpeg )  ##Array containing matched files, mention where to search and what files here
 old=""
 foo=1
 for i in "${files[@]}"; do 
@@ -15,6 +15,6 @@ for i in "${files[@]}"; do
         foo=1
     fi
     f="${i##*/}"  ##gets the file name
-        #echo "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d" )_"${foo}".jpg"
+        echo "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d" )_"${foo}".jpg"
         mv -n "$i" "$d"/"${d_}"_"$(date -r "$i" +"%Y%m%d" )_"${foo}".jpg"
 done
